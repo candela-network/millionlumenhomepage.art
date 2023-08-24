d8dcf5db6d0644fc12185357c4da78d1ec7b3563
Vincent Amouret <vincent.amouret@gmail.com>
2023-08-24 13:14:32 +0000

    somehow fix the png error in preview mode

--- mlh-frontend/src/pages/fresque.png.ts
+++ mlh-frontend/src/pages/fresque.png.ts
@@ -17,10 +17,11 @@ export async function get({params, request}) {
     x.set(now);
   } else {
     console.log("from cache")
+    //fresque = await update();
     fresque = await Jimp.read('./data/data-fresque.png');
   }
   return {
-    body: await fresque.getBufferAsync("image/png"),
+    body: "data:image/png;base64," + (await fresque.getBufferAsync("image/png")).toString("base64"),
   };
 }
 
@@ -34,9 +35,8 @@ async function update() {
       let data = JSON.parse(await fs.readFile(filename, "utf8"));
       let b64 = data.image.substring(data.image.indexOf(',')+1);
       let image = b64 != data.image ? await Jimp.read(Buffer.from(b64, "base64")) : await Jimp.read(data.image);
-
-      let xy = JSON.parse(await fs.readFile(filename, "utf8")).coords;
-      fresque.composite(image, xy[0] * 16, xy[1] * 16);
+      let xy = data.coords;
+       fresque.composite(image, xy[0] * 16, xy[1] * 16);
     } catch (e) {
       //
       console.log(e)
